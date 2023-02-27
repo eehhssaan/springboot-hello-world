da02357a3d600c71f6d915132ca812e7baf7a8c4
eehhssaan <ehsani786@live.com>
2023-02-27 22:52:00 +0800

    complete project

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,6 +2,8 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 public class HelloWorldApplication {
@@ -10,4 +12,11 @@ public class HelloWorldApplication {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String home(){
+        return "Welcome Home (TechNet)";
+    }
+
 }
+
+
